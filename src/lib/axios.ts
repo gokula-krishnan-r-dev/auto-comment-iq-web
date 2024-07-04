b13e4bddbc61929b87e38df9072d55cc5a615335
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-07-04 10:32:44 +0530

    chore: Update API base URL to use production endpoint

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,8 +4,8 @@ import Axios from "axios";
 
 // Immediately-invoked function to determine if we are server-side
 const axios = Axios.create({
-  baseURL: "http://localhost:3000/v1",
-  // baseURL: "https://autocommentapi.vercel.app/v1",
+  // baseURL: "http://localhost:3000/v1",
+  baseURL: "https://autocommentapi.vercel.app/v1",
 });
 
 export default axios;
